e5503a17de55724772e4ebed6222b5604f17706a
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-03 09:00:08 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,345 @@
+import os
+import time
+import math
+import json
+import string
+import random
+import traceback
+import asyncio
+import datetime
+import aiofiles
+import datetime
+from countryinfo import CountryInfo
+from random import choice 
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent
+from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid, UserNotParticipant, UserBannedInChannel
+from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
+from database import Database
+
+
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+BOT_OWNER = int(os.environ["BOT_OWNER"])
+DATABASE_URL = os.environ["DATABASE_URL"]
+db = Database(DATABASE_URL, "FnCountryInfoBot")
+broadcast_ids = {}
+
+
+Bot = Client(
+    "Country Info Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+)
+
+START_TEXT = """Hello {} 😌
+I am a country information finder bot.
+
+>> `I can find information of any country of the world.`
+
+Made by @FayasNoushad"""
+
+HELP_TEXT = """**Hey, Follow these steps:**
+
+➠ Just send me a country name 
+➠ Then I will check and send you the informations
+
+**Available Commands**
+
+/start - Checking Bot Online
+/help - For more help
+/about - For more about me
+/status - For bot status
+
+Made by @FayasNoushad"""
+
+ABOUT_TEXT = """--**About Me**-- 😎
+
+🤖 **Name :** [Country Info Bot](https://telegram.me/{})
+👨‍💻 **Developer :** [Fayas](https://github.com/FayasNoushad)
+📢 **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
+👥 **Group :** [Developer Team](https://telegram.me/TheDeveloperTeam
+🌐 **Source :** [👉 Click here](https://github.com/FayasNoushad/Country-Info-Bot-V2)
+📝 **Language :** [Python3](https://python.org)
+🧰 **Framework :** [Pyrogram](https://pyrogram.org)
+📡 **Server :** [Heroku](https://heroku.com)"""
+
+FORCE_SUBSCRIBE_TEXT = "<code>Sorry Dear You Must Join My Updates Channel for using me 😌😉....</code>"
+
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⚙ Help', callback_data='help'),
+        InlineKeyboardButton('About 🔰', callback_data='about'),
+        InlineKeyboardButton('Close ✖️', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏘 Home', callback_data='home'),
+        InlineKeyboardButton('About 🔰', callback_data='about'),
+        InlineKeyboardButton('Close ✖️', callback_data='close')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏘 Home', callback_data='home'),
+        InlineKeyboardButton('Help ⚙', callback_data='help'),
+        InlineKeyboardButton('Close ✖️', callback_data='close')
+        ]]
+    )
+ERROR_BUTTON = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⚙ Help', callback_data='help'),
+        InlineKeyboardButton('Close ✖️', callback_data='close')
+        ]]
+    )
+
+
+@Bot.on_callback_query()
+async def cb_handler(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            reply_markup=START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            reply_markup=HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT.format((await bot.get_me()).username),
+            reply_markup=ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+    else:
+        await update.message.delete()
+
+
+@Bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    if not await db.is_user_exist(update.from_user.id):
+	    await db.add_user(update.from_user.id)
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+	reply_markup=START_BUTTONS
+    )
+
+
+@Bot.on_message(filters.private & filters.command(["help"]))
+async def help(bot, update):
+    if not await db.is_user_exist(update.from_user.id):
+	    await db.add_user(update.from_user.id)
+    await update.reply_text(
+        text=HELP_TEXT,
+      	disable_web_page_preview=True,
+	reply_markup=HELP_BUTTONS
+    )
+
+
+@Bot.on_message(filters.private & filters.command(["about"]))
+async def about(bot, update):
+    if not await db.is_user_exist(update.from_user.id):
+	    await db.add_user(update.from_user.id)
+    await update.reply_text(
+        text=ABOUT_TEXT.format((await bot.get_me()).username),
+        disable_web_page_preview=True,
+	reply_markup=ABOUT_BUTTONS
+    )
+
+
+def country_info(country):
+    country = CountryInfo(country)
+    info = f"""
+Name : `{country.name()}`
+Native Name : `{country.native_name()}`
+Capital : `{country.capital()}`
+Population : `{country.population()}`
+Region : `{country.region()}`
+Sub Region : `{country.subregion()}`
+Top Level Domains : `{country.tld()}`
+Calling Codes : `{country.calling_codes()}`
+Currencies : `{country.currencies()}`
+Residence : `{country.demonym()}`
+Timezone : `{country.timezones()}`
+"""
+    wiki = country.wiki()
+    country_name = country.name().replace(" ", "+")
+    google = "https://www.google.com/search?q=" + country_name
+    return info, wiki, google
+
+
+@Bot.on_message(filters.private & filters.text)
+async def countryinfo(bot, update):
+    if not await db.is_user_exist(update.from_user.id):
+	    await db.add_user(update.from_user.id)
+    if UPDATE_CHANNEL:
+        try:
+            user = await bot.get_chat_member(UPDATE_CHANNEL, update.chat.id)
+            if user.status == "kicked":
+                await update.reply_text(text="You are banned!")
+                return
+        except UserNotParticipant:
+            await update.reply_text(
+		  text=FORCE_SUBSCRIBE_TEXT,
+		  reply_markup=InlineKeyboardMarkup(
+			  [[InlineKeyboardButton(text="😎 Join Channel 😎", url=f"https://telegram.me/{UPDATE_CHANNEL}")]]
+		  )
+	    )
+            return
+        except Exception as error:
+            print(error)
+            await update.reply_text(text="Something wrong. Contact <a href='https://telegram.me/TheFayas'>Developer</a>.", disable_web_page_preview=True)
+            return
+    info, wiki, google = country_info(update.text)
+    reply_markup=InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Wikipedia', url=wiki),
+        InlineKeyboardButton('Google', url=google)
+        ],[
+        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
+        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+        ]]
+    )
+    try:
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=info,
+            reply_markup=reply_markup,
+            disable_web_page_preview=True,
+            reply_to_message_id=update.message_id
+        )
+    except FloodWait as floodwait:
+        await asyncio.sleep(floodwait.x)
+        return countryinfo(bot, update)
+    except KeyError as keyerror:
+        print(keyerror)
+    except Exception as error:
+        print(error)
+
+
+@Bot.on_inline_query()
+async def countryinfo_inline(bot, update):
+    join_channel_text = "Please join my channel for more bots and updates"
+    channel_reply_markup=InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('😎 Join Channel 😎', url='https://telegram.me/FayasNoushad')
+        ]]
+    )
+    if UPDATE_CHANNEL:
+        try:
+            user = await bot.get_chat_member(UPDATE_CHANNEL, update.chat.id)
+            if user.status == "kicked":
+                return
+        except UserNotParticipant:
+            await bot.answer_inline_query(
+		inline_query_id=update.chat.id,
+		results=[
+                    InlineQueryResultArticle(
+                        title="Join Channel 😎",
+                        description=join_updates_channel_text,
+                        input_message_content=InputTextMessageContent(FORCE_SUBSCRIBE_TEXT),
+                        reply_markup=InlineKeyboardMarkup(
+                            [[InlineKeyboardButton(text="😎 Join Channel 😎", url=f"https://telegram.me/{UPDATE_CHANNEL}")]]
+                        )
+                    )
+                ]
+            )
+            return 
+        except Exception as error:
+            print(error)
+            return 
+    info, wiki, google = country_info(update.query)
+    reply_markup=InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Wikipedia', url=wiki),
+        InlineKeyboardButton('Google', url=google)
+        ],[
+        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
+        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+        ]]
+    )
+    if update.query == "":
+        answers=[
+            InlineQueryResultArticle(
+                title="Join Channel 😎",
+                description=join_channel_text,
+                input_message_content=InputTextMessageContent(join_channel_text),
+                reply_markup=channel_reply_markup
+            )
+        ]
+    else:
+        answers=[
+            InlineQueryResultArticle(
+                title=update.query,
+                description=f"Information of {update.query}",
+                input_message_content=InputTextMessageContent(info),
+                reply_markup=reply_markup
+            )
+        ]
+    await bot.answer_inline_query(
+        inline_query_id=update.chat.id,
+        results=answers
+    )
+
+
+@Bot.on_message(filters.private & filters.command("broadcast") & filters.user(BOT_OWNER) & filters.reply, group=4)
+async def broadcast(bot, update):
+	all_users = await db.get_all_users()
+	broadcast_msg = update.reply_to_message
+	while True:
+	    broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+	    if not broadcast_ids.get(broadcast_id):
+	        break
+	out = await update.reply_text(text=f"Broadcast Started! You will be notified with log file when all the users are notified.")
+	start_time = time.time()
+	total_users = await db.total_users_count()
+	done = 0
+	failed = 0
+	success = 0
+	broadcast_ids[broadcast_id] = dict(total = total_users, current = done, failed = failed, success = success)
+	async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
+	    async for user in all_users:
+	        sts, msg = await send_msg(user_id = int(user['id']), message = broadcast_msg)
+	        if msg is not None:
+	            await broadcast_log_file.write(msg)
+	        if sts == 200:
+	            success += 1
+	        else:
+	            failed += 1
+	        if sts == 400:
+	            await db.delete_user(user['id'])
+	        done += 1
+	        if broadcast_ids.get(broadcast_id) is None:
+	            break
+	        else:
+	            broadcast_ids[broadcast_id].update(dict(current = done, failed = failed, success = success))
+	if broadcast_ids.get(broadcast_id):
+	    broadcast_ids.pop(broadcast_id)
+	completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
+	await asyncio.sleep(3)
+	await out.delete()
+	if failed == 0:
+	    await update.reply_text(text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.", quote=True)
+	else:
+	    await update.reply_document(document='broadcast.txt', caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.")
+	os.remove('broadcast.txt')
+
+
+@Bot.on_message(filters.private & filters.command("status"), group=5)
+async def status(bot, update):
+    total_users = await db.total_users_count()
+    text = "**Bot Status**\n"
+    text += f"\n**Total Users:** `{total_users}`"
+    await update.reply_text(
+        text=text,
+        quote=True,
+        disable_web_page_preview=True
+    )
+
+
+Bot.run()
