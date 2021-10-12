e59512e5eedd24634b9109301746e8e03d7212f1
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-12 19:54:42 +0530

    Added `send_msg` defenition

--- main.py
+++ main.py
@@ -295,6 +295,23 @@ async def countryinfo_inline(bot, update):
     )
 
 
+async def send_msg(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} : deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} : blocked the bot\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} : user id invalid\n"
+    except Exception as e:
+        return 500, f"{user_id} : {traceback.format_exc()}\n"
+
+
 @Bot.on_message(filters.private & filters.command("broadcast") & filters.user(BOT_OWNER) & filters.reply, group=1)
 async def broadcast(bot, update):
 	broadcast_ids = {}
